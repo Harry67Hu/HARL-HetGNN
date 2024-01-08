b59f25f616f0d674149b810f69bb6cb4688e483f
Harry67Hu <hutianyi2021@ia.ac.cn>
2024-01-08 22:15:32 +0800

    for better use

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义基础算法和实验场景
-BASE_ALGO="HAMA"
+BASE_ALGO="E-GAT"
 EXPERIMENT_SCENE="MMM2"
 
 # 循环创建tmux会话并执行命令
@@ -17,6 +17,12 @@ do
     # 激活conda环境并执行脚本
     tmux send-keys -t $SESSION_NAME "conda activate harl" C-m
     tmux send-keys -t $SESSION_NAME "python train.py --algo mappo-HOANet --env smac --exp_name MMM_model --seed $SEED" C-m
+
+    echo "创建了第${i}个tmux会话。"
+    # 等待30秒,防止进程冲突占用
+    if [ $i -ne 3 ]; then
+        sleep 30s
+    fi
 done
 
 echo "脚本执行完毕。创建了3个tmux会话。"
